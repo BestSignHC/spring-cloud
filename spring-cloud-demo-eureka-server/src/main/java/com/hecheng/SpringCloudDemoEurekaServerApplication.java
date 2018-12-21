95fdee5f2a624cd6c59b565f3167c09668db1226
BestSignHC <hust_hc@126.com>
2018-12-21 11:03:11 +0800

    eureka service discovery

---
+++ spring-cloud-demo-eureka-server/src/main/java/com/hecheng/SpringCloudDemoEurekaServerApplication.java
@@ -0,0 +1,16 @@
+package com.hecheng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SpringCloudDemoEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudDemoEurekaServerApplication.class, args);
+	}
+
+}
+
